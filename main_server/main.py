b34aff9f70127efa8773a56291823656d9417eab
shombal1 <sasha.ruchits.06@gmail.com>
2025-05-03 15:24:53 +0300

    Add URL generation router to main application

--- main_server/main.py
+++ main_server/main.py
@@ -22,6 +22,8 @@ app.include_router(main_server.api.routers.auth.router, prefix='/api')
 app.include_router(main_server.api.routers.test.router, prefix='/api')
 app.include_router(main_server.api.routers.user.router, prefix='/api')
 
+app.include_router(main_server.api.routers.url_generate.router, prefix='/api')
+
 @app.on_event("startup")
 async def startup_event():
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
