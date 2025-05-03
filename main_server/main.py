98d6ea15f35253a6c0778cccfb40631f5293a026
shombal1 <sasha.ruchits.06@gmail.com>
2025-05-03 15:21:23 +0300

    Add CORS middleware configuration to FastAPI application

--- main_server/main.py
+++ main_server/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 import os
+
+from starlette.middleware.cors import CORSMiddleware
+
 import main_server.api.routers.reports
 
 UPLOAD_FOLDER = os.path.abspath('../uploads')
 
 app = FastAPI()
-
+# TODO add env fro allow domen
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Разрешить все домены (небезопасно для прода!)
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы (GET, POST, PUT и т.д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 app.include_router(main_server.api.routers.reports.router, prefix='/api')
 app.include_router(main_server.api.routers.auth.router, prefix='/api')
 app.include_router(main_server.api.routers.test.router, prefix='/api')
